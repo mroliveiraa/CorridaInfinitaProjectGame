9407c31a09b2db3a3c55db3529e1c67e9977403f
mathe <mathe@192.168.0.180>
2025-12-01 22:22:37 -0300

    adicionando fundo de seleção de personagens, deixando os botões transparentes

--- CorridaInfinitaGame/src/Personagem/Player.java
+++ CorridaInfinitaGame/src/Personagem/Player.java
@@ -4,6 +4,7 @@ package Personagem;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
+import javax.swing.ImageIcon;
 import java.awt.Rectangle; // Importei Rectangle para garantir que não haja erros
 
 public class Player {
@@ -14,20 +15,36 @@ public class Player {
     protected boolean estaPulando;//Estado
     protected final int distanciaChao = 400;//altura do solo após pular
 
-            
+     //Animação
+    private Image[] frames; //Vetores dos sprites
+    private int frameAtual = 0; // Frame exibido
+    private int contadorFrames = 0; // comtrola vel da troca de frames
+   
     
-    //construtor
+
+//construtor
     public Player (int startX, int startY){
     this.x  = startX;
     this.y = startY;
     this.velocidadeY = 0;  //velocidade do pulo
     this.estaPulando = false;
      
+    carregarSprintes();
     }
     
+    //Carregar os frames
+    private void carregarSprintes() {
+        frames = new Image[3];
+        
+        frames[0] = new ImageIcon(getClass().getResource("/res/FrameMov1.png")).getImage();
+        frames[1] = new ImageIcon(getClass().getResource("/res/FrameMov2.png")).getImage();
+        frames[2] = new ImageIcon(getClass().getResource("/res/FrameMov3.png")).getImage();
+    }
     
-    //inicia na posição
+    //Lógica da movimentação
     public void Update() {
+       
+        //pulo
         if (estaPulando) {
             velocidadeY += 1.3; //gravidade do pulo
             y += velocidadeY;
@@ -39,30 +56,42 @@ public class Player {
                 velocidadeY = 0;
                 
                 
-            } 
+                } 
             }
+        
+    
+        //Animação
+        animar();
+    }
+    
+    //Automatização da troca de frame 
+    private void animar() {
+        contadorFrames++;
+        
+        if (contadorFrames >= 10) {
+            frameAtual++;
+            if (frameAtual >= frames.length) {
+                frameAtual = 0;//Volta ao primeiro'LOOP'
+            }
+            contadorFrames = 0;
         }
-                
-            public java.awt.Rectangle getBounds() {
-    return new java.awt.Rectangle(x, y, 50, 50);
+    }
+    
+    public java.awt.Rectangle getBounds() {
+      return new java.awt.Rectangle(x, y, 50, 50);
 }
 
     
             public void Pulo (){
                 if (!estaPulando){
                     estaPulando = true;
-                    velocidadeY = -26; 
+                    velocidadeY = -30; 
                    }   
                 }   
+            
+            //desenha o sprite atual
                 public void imprimirPersonagem (Graphics g){ 
-          g.setColor (Color.BLUE);
-          g.fillRect(x, y, 50, 50);
-          
-        
-    
-    
-    
-    
+          g.drawImage(frames[frameAtual], x, y, 70, 70, null);
+       }
+    }
 
-}
-}
